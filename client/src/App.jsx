ffe4572f80e79d2244cc34a5b637bb66954c0e0c
Chris McVey <chris.mcvey@gmail.com>
2021-05-12 20:01:47 -0700

    File structure for redux set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>React is working!</div>
+    <div>React is rendering properly to the DOM.
+      Welcome to the Vegan Roadshow!
+    </div>
   )
 }
 
